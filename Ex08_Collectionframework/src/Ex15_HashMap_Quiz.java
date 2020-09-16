e592cac45f74549d462379a84a3d3fb77cfc5528
HeeSeungPaek <tivkfzlzl@gmail.com>
2020-09-16 22:33:36 +0900

    start up for re-initialize

--- Ex08_Collectionframework/src/Ex15_HashMap_Quiz.java
+++ Ex08_Collectionframework/src/Ex15_HashMap_Quiz.java
@@ -28,12 +28,16 @@ public class Ex15_HashMap_Quiz {
 
 		for (int i = 0; i < quiz.size(); i++) {
 			Set set = quiz.keySet(); // keySet() : Set을 구현하고 있는 객체의 주소를 리턴
+			System.out.println(set.toArray());
 			Object[] obj = set.toArray(); // http://asuraiv.blogspot.com/2015/06/java-list-toarray.html
 			// System.out.println(obj.length);
 			System.out.println("문제:" + obj[i]);
 			System.out.println(quiz.keySet().toArray()[i]);
 
-			System.out.println(quiz.get(quiz.keySet().toArray()[i])); // 완벽하게 모르겠음 이건
+			System.out.println(quiz.get(quiz.keySet().toArray()[i]));
+			//quiz.keySet().toArray()[i] > quiz맵의 키값들을 내부적으로 new Set하여 키 객체들을 Set에
+			//넣고, 그 키들을 배열로 변환해 [i]번 방의 키값들에 대해
+			// quiz.get(키) >> 인자 자리의 키에 해당하는 Value를 볼 수 있게된다.
 		}
 
 	}
