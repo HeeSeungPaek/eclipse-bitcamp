006d5a9b9fe9c5db7e19dfe4ed27abeb0c183793
HeeSeungPaek <tivkfzlzl@gmail.com>
2020-09-16 22:41:25 +0900

    re-initializing repo

--- Project(JAVA)_2/src/kr/or/bit_2/Bicycle.java
+++
@@ -1,9 +0,0 @@
-package kr.or.bit_2;
-
-import java.util.HashMap;
-
-public class Bicycle {
-	static int count;
-	
-	
-}
--- Project(JAVA)_2/src/kr/or/bit_2/main.java
+++
@@ -1,9 +0,0 @@
-package kr.or.bit_2;
-
-public class main {
-	public static void main(String[] args) {
-		AccountManager am =new AccountManager();
-		am.startLogIn();
-		
-	}
-}
